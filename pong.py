1df762539831a0b74caa3c8df2c6d78ba3c434da
Egr228 <75569274+Egr228@users.noreply.github.com>
2023-05-13 08:31:10 +0300

    Add files via upload

--- pong.py
+++ pong.py
@@ -17,6 +17,9 @@ class GameSprite(sprite.Sprite):
         if keypressed[K_s] and self.rect.y >= 0:
             self.rect.x -= self.speed
 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 x_hero = 400
 y_hero = 400
 speed_hero = 10
@@ -28,8 +31,11 @@ FPS = 60
 clock = time.Clock()
 
 window = display.set_mode((1000,625))
-display.set_caption('Just_pong')
+display.set_caption('JoJong’s Bizarre Adventure')
 background = transform.scale(image.load('d.jpg'),(1000,625))
+
+game_sprite = GameSprite('original-6005178dc77cc.jpg',200, 200, x_hero, y_hero, speed_hero)
+game_sprite2 = GameSprite('Ha877df28b2954cb18ba0ceb42b0ff9a09.png',200, 400, x_hero, y_hero, speed_hero)
 
 game = True
 while game:
@@ -37,6 +43,8 @@ while game:
         if el.type == QUIT:
             game = False
     window.blit(background,(0,0))
+    game_sprite.reset()
+
 
     clock.tick(FPS)
     time.delay(5)
